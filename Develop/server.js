6894d9677f6d7c94ed74bf6f37fd2d2d56047d05
Aleenabrink93 <aleenabrink@gmail.com>
2023-07-01 12:00:38 -0600

    routed notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,11 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
