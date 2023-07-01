5130b999cfcbbc9a64d109fd55d52e3c638ba11b
Aleenabrink93 <aleenabrink@gmail.com>
2023-07-01 16:22:04 -0600

    added HTML route and API notes route

--- Develop/server.js
+++ Develop/server.js
@@ -8,12 +8,24 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/api/notes',(req,res)=>{
+res.json(savedNotes)
+})
+app.post('/api/notes',(req,res)=>{
+
+})
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
 
 
 app.listen(PORT, () =>
